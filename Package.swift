28ea3f36bad2973917aa0c1c3e4cecf2a94d0e2c
Tanuja Rao <tanuja.rao@loylty.com>
2024-07-26 16:57:47 +0530

    added package

--- Package.swift
+++ Package.swift
@@ -5,19 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "SPMTestRepo",
+    platforms: [
+        .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SPMTestRepo",
             targets: ["SPMTestRepo"]),
     ],
+//    dependencies: [
+//        .package(url: "", branch: "")
+//    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SPMTestRepo"),
+//        .target(
+//            name: "SPMTestRepo"),
         .testTarget(
             name: "SPMTestRepoTests",
             dependencies: ["SPMTestRepo"]),
+        .binaryTarget(name: "SPMTestRepo", path: "./Sources/SPMFrameworkTest.xcframework")
     ]
 )
---
+++ Sources/SPMFrameworkTest.xcframework/ios-arm64_x86_64-simulator/SPMFrameworkTest.framework/Headers/SPMFrameworkTest.h
@@ -0,0 +1,18 @@
+//
+//  SPMFrameworkTest.h
+//  SPMFrameworkTest
+//
+//  Created by Tanuja Rao on 26/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SPMFrameworkTest.
+FOUNDATION_EXPORT double SPMFrameworkTestVersionNumber;
+
+//! Project version string for SPMFrameworkTest.
+FOUNDATION_EXPORT const unsigned char SPMFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SPMFrameworkTest/PublicHeader.h>
+
+
